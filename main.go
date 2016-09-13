27169550fc0ed74ea3060d83ca84dda81aad2ca8
Kentaro Kawano <kentaro.kawano@gmail.com>
2016-09-13 13:00:45 +0900

    編集中

--- main.go
+++ main.go
@@ -13,26 +13,41 @@ import (
 	"time"
 )
 
+var checkDuration = time.Duration(10) * time.Minute
+
 // Config は更新対象の設定。複数の設定を保持している。
 type Config struct {
-	// Tokens はAuthTokenのスライス
-	Tokens []*AuthToken
+	Authentications []*Authentication
 }
 
-// AuthToken は認証用のTokenと関連の情報
-type AuthToken struct {
-	Name             string
-	ClientID         string
-	ClientSecret     string
-	AuthURL          string
-	TokenURL         string
-	AccessToken      string `json:"access_token"`
-	ExpiresIn        int    `json:"expires_in"`
-	RefreshToken     string `json:"refresh_token"`
-	ExpiresAt        time.Time
-	Error            string
+// AuthResponse は認証のレスポンス解釈用の構造体です
+type AuthResponse struct {
+	RefreshToken string `json:"refresh_token"`
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    int    `json:"expires_in"`
+
+	Error            string `json:"error"`
 	ErrorDescription string `json:"error_description"`
-	Scopes           []string
+}
+
+// Authentication は認証の内容を保持する構造体です
+type Authentication struct {
+	Name string
+
+	ClientID     string
+	ClientSecret string
+
+	RefreshToken string
+	AccessToken  string
+	ExpiresIn    int
+	ExpiresAt    time.Time
+
+	// Authorization用
+	AuthURL string
+	Scopes  []string
+
+	// Token取得用
+	TokenURL string
 }
 
 func loadConf() (*Config, error) {
@@ -53,15 +68,15 @@ func writeConf(conf *Config) error {
 	return ioutil.WriteFile("conf.toml", buffer.Bytes(), os.ModePerm)
 }
 
-func refreshAccessToken(token *AuthToken) error {
+func refreshAccessToken(auth *Authentication) error {
 
 	values := url.Values{}
-	values.Add("client_id", token.ClientID)
-	values.Add("client_secret", token.ClientSecret)
-	values.Add("refresh_token", token.RefreshToken)
+	values.Add("client_id", auth.ClientID)
+	values.Add("client_secret", auth.ClientSecret)
+	values.Add("refresh_token", auth.RefreshToken)
 	values.Add("grant_type", "refresh_token")
 
-	resp, err := http.PostForm(token.TokenURL, values)
+	resp, err := http.PostForm(auth.TokenURL, values)
 
 	if err != nil {
 		return err
@@ -73,39 +88,39 @@ func refreshAccessToken(token *AuthToken) error {
 		return err
 	}
 
-	var at AuthToken
-	err = json.Unmarshal(b, &at)
+	var ar AuthResponse
+	err = json.Unmarshal(b, &ar)
 	if err != nil {
 		return err
 	}
 
-	if at.Error != "" {
-		return fmt.Errorf("response token error: %s, %s", at.Error, at.ErrorDescription)
+	if ar.Error != "" {
+		return fmt.Errorf("response token error: %s, %s", ar.Error, ar.ErrorDescription)
 	}
 
-	if at.AccessToken == "" {
+	if ar.AccessToken == "" {
 		return fmt.Errorf("response token error: empty access token")
 	}
 
-	token.AccessToken = at.AccessToken
-	if at.RefreshToken != "" {
-		token.RefreshToken = at.RefreshToken
+	auth.AccessToken = ar.AccessToken
+	if ar.RefreshToken != "" {
+		auth.RefreshToken = ar.RefreshToken
 	}
-	token.ExpiresAt = time.Now().Add(time.Duration(at.ExpiresIn) * time.Second)
+	auth.ExpiresAt = time.Now().Add(time.Duration(ar.ExpiresIn) * time.Second)
 
 	return nil
 }
 
-func getAccessToken(token *AuthToken) error {
+func getAccessToken(req *Authentication) error {
 
 	values := url.Values{}
 	values.Add("response_code", "code")
-	values.Add("client_id", token.ClientID)
-	values.Add("client_secret", token.ClientSecret)
+	values.Add("client_id", req.ClientID)
+	values.Add("client_secret", req.ClientSecret)
 	values.Add("redirect_url", "urn:ietf:wg:oauth:2.0:oob")
-	values.Add("scope", strings.Join(token.Scopes, " "))
+	values.Add("scope", strings.Join(req.Scopes, " "))
 
-	authorizeURL := fmt.Sprintf("%s?%s", token.AuthURL, values.Encode())
+	authorizeURL := fmt.Sprintf("%s?%s", req.AuthURL, values.Encode())
 
 	fmt.Printf("Open url: %s\n", authorizeURL)
 	fmt.Printf("Enter authorization code: ")
@@ -120,8 +135,10 @@ func getAccessToken(token *AuthToken) error {
 
 }
 
-func main() {
+func doNew() {
+}
 
+func doRefresh() {
 	conf, err := loadConf()
 	if err != nil {
 		fmt.Println(err)
@@ -129,17 +146,38 @@ func main() {
 	}
 
 	var updated bool
-	for _, token := range conf.Tokens {
-		if token.ExpiresAt.Before(time.Now().Add(-1 * time.Duration(10) * time.Minute)) {
-			err := refreshAccessToken(token)
+	for _, auth := range conf.Authentications {
+		// チェック期限だったらチェックする
+		if auth.ExpiresAt.Before(time.Now().Add(-1 * checkDuration)) {
+			err := refreshAccessToken(auth)
 			if err != nil {
+				fmt.Println(err)
 				break
 			}
 			updated = true
 		}
 	}
+
 	if updated {
-		writeConf(conf)
+		err = writeConf(conf)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func main() {
+
+	if len(os.Args) == 1 {
+		doRefresh()
+		return
+	}
+
+	switch os.Args[1] {
+	case "new":
+		doNew()
+	case "refresh":
+		doRefresh()
 	}
 
 }
